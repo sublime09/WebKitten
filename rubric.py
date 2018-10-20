a385a189214076d9a709689886d95cb81c17ec4a
Patrick <sublime@vt.edu>
2018-10-20 16:36:55 -0400

    skeleton

--- rubric.py
+++ rubric.py
@@ -1,33 +1,38 @@
 class Rubric:
-	def __init__(self):
-		self.crits = dict()
+    def __init__(self):
+        self.crits = dict()
 
-	def addCriteria(self, key, problem, penalty, comment):
-		c = Criteria()
-		# c.key = key
-		c.problem = problem
-		c.penalty = penalty
-		c.comment = comment
-		self.crits[key] = c
+    def addCrit(self, key, problem, penalty, comment):
+        c = Criteria()
+        c.key = key
+        c.problem = problem
+        c.penalty = penalty
+        c.comment = comment
+        self.crits[key] = c
 
-	def __repr__(self):
-		s = []
-		for k, v in self.crits.items():
-			s.append("{} :: {}".format(k, v))
-		return '\n'.join(s)
+    def __repr__(self):
+        s = []
+        for k, v in self.crits.items():
+            s.append("{} :: {}".format(k, v))
+        return '\n'.join(s)
+
 
 class Criteria:
-	def __repr__(self):
-		assert self.problem, "Problem not assigned"
-		assert self.penalty, "Penalty not assigned"
-		assert self.comment, "Comment not assigned"
-		args = (self.problem, self.penalty, self.comment)
-		return "P{}: {}pts: {}".format(*args)
+    def __repr__(self):
+        assert self.problem, "Problem not assigned"
+        assert self.penalty, "Penalty not assigned"
+        assert self.comment, "Comment not assigned"
+        args = (self.problem, self.penalty, self.comment)
+        return "P{}: {}pts: {}".format(*args)
+
 
 r = Rubric()
-r.addCriteria("k", 1, -5, "problem???")
-r.addCriteria("l", 1, -5, "problem")
-r.addCriteria("m", 1, -5, "PROBLEM")
-
-print(r)
-
+if __name__ == '__main__':
+    r.addCrit("k", 1, -5, "problem???")
+    r.addCrit("l", 1, -5, "problem")
+    r.addCrit("m", 1, -5, "PROBLEM")
+    print(r)
+else:
+    r.addCrit("k", 1, -5, "problem???")
+	
+    
