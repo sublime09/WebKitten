cfa60c34bbef83eeabfd76480836a315d283812c
Patrick Sullivan <sublime@vt.edu>
2018-10-24 17:16:41 -0400

    use tabs

--- main.py
+++ main.py
@@ -6,27 +6,27 @@ rubric = rubric.getHW6()
 
 def main():
 	usage = "Commands: exit, help, rubric, or a string of rubric keys"
-    print(usage)
-    userIn = ""
-    while True:
-        userIn = input("Input: ").lower().strip()
-        if "exit" in userIn:
-            break
-        elif "help" in userIn:
-        	print(usage)
-            print("Enter 'exit' to exit at any time.")
-        elif 'rubric' in userIn:
-        	print(rubric)
-        else:
-            comment = []
-            for c in userIn:
-                comment.append(rubric.get(c))
-            comment = '\n'.join(comment)
-            pyperclip.copy(comment)
-            print("Copied to clipboard:")
-            print(comment)
-            print()
+	print(usage)
+	userIn = ""
+	while True:
+		userIn = input("Input: ").lower().strip()
+		if "exit" in userIn:
+			break
+		elif "help" in userIn:
+			print(usage)
+			print("Enter 'exit' to exit at any time.")
+		elif 'rubric' in userIn:
+			print(rubric)
+		else:
+			comment = []
+			for c in userIn:
+				comment.append(rubric.get(c))
+			comment = '\n'.join(comment)
+			pyperclip.copy(comment)
+			print("Copied to clipboard:")
+			print(comment)
+			print()
 
 
 if __name__ == '__main__':
-    main()
+	main()
